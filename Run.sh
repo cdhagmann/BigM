7acf562834ecd50652521f0c78771cac014c64cb
Christopher Hagmann <cdhagmann@gmail.com>
2014-12-19 07:13:00 -0500

    Run.sh to run parallel code

---
+++ Run.sh
@@ -0,0 +1,17 @@
+./CL_PMaster.sh M
+
+for i in {1..10}
+do
+    ./CL_PMaster.sh S
+
+for i in {1..10}
+do
+    ./CL_PMaster.sh P
+done
+
+for i in {1..10}
+do
+    ./CL_PMaster.sh M
+done
+
+./CL_PMaster.sh L
